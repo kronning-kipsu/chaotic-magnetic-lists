8044f5930f21abfea214fc2106eb269977cc8cf0
Kyle Ronning <kyleronning@gmail.com>
2020-09-16 13:42:29 -0500

    Add AppDispatch type and typed useDispatch

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,19 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+import { useDispatch } from "react-redux"
+import counterReducer from '../features/counter/counterSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
-});
+})
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch = () => useDispatch<AppDispatch>()
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
   unknown,
   Action<string>
->;
+>
